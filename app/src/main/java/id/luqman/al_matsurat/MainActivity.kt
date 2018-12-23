7788d9d0a8647ac88b642d4a2d262239b773d179
Luqman Sungkar <loekmansungkar@gmail.com>
2018-12-23 19:25:13 +0700

    init view pager with doa pages content

--- app/src/main/java/id/luqman/al_matsurat/MainActivity.kt
+++ app/src/main/java/id/luqman/al_matsurat/MainActivity.kt
@@ -1,12 +1,21 @@
 package id.luqman.al_matsurat
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import id.luqman.al_matsurat.adapter.DoaPagerAdapter
+import id.luqman.al_matsurat.data.DataHelper
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pagerAdapter = DoaPagerAdapter(supportFragmentManager, DataHelper.getDoaPages(this))
+
+        val viewPager: ViewPager = find(R.id.doa_viewpager)
+        viewPager.adapter = pagerAdapter
     }
 }
