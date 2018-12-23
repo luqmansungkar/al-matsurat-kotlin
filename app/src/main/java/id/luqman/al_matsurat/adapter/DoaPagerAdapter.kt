a73263a9795a19a78e4fa24019ccbd8715b18710
Luqman Sungkar <loekmansungkar@gmail.com>
2018-12-23 19:27:48 +0700

    change to fragment state pager adapter

--- app/src/main/java/id/luqman/al_matsurat/adapter/DoaPagerAdapter.kt
+++ app/src/main/java/id/luqman/al_matsurat/adapter/DoaPagerAdapter.kt
@@ -2,11 +2,11 @@ package id.luqman.al_matsurat.adapter
 
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.FragmentStatePagerAdapter
 import id.luqman.al_matsurat.data.Page
 import id.luqman.al_matsurat.fragment.DoaPageFragment
 
-class DoaPagerAdapter(fm: FragmentManager, private val pages: List<Page>): FragmentPagerAdapter(fm){
+class DoaPagerAdapter(fm: FragmentManager, private val pages: List<Page>): FragmentStatePagerAdapter(fm){
     override fun getItem(position: Int): Fragment {
         return DoaPageFragment.newInstance(pages[position])
     }
