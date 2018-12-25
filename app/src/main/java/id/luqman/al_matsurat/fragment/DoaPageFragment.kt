96805cac8515372796a0c0181a2a874dbf1c8348
Luqman Sungkar <loekmansungkar@gmail.com>
2018-12-25 17:16:10 +0700

    set arabic font

--- app/src/main/java/id/luqman/al_matsurat/fragment/DoaPageFragment.kt
+++ app/src/main/java/id/luqman/al_matsurat/fragment/DoaPageFragment.kt
@@ -1,14 +1,16 @@
 package id.luqman.al_matsurat.fragment
 
+import android.content.res.AssetManager
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import id.luqman.al_matsurat.R
 import id.luqman.al_matsurat.data.Page
 import org.jetbrains.anko.support.v4.find
-import id.luqman.al_matsurat.R
 
 class DoaPageFragment: Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -16,14 +18,15 @@ class DoaPageFragment: Fragment(){
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        val title: TextView = find(R.id.doa_title)
         val arabic: TextView = find(R.id.doa_arabic)
         val translation: TextView = find(R.id.doa_translation)
 
         val args = arguments
-        title.text = args?.getString("title")
         arabic.text = args?.getString("arabic")
         translation.text = args?.getString("translation")
+        val typeface = Typeface.createFromAsset(this@DoaPageFragment.context?.assets as AssetManager, "utsmani.otf")
+        arabic.typeface = typeface
+
         super.onActivityCreated(savedInstanceState)
     }
 
@@ -33,7 +36,7 @@ class DoaPageFragment: Fragment(){
             args.putString("title", page.title)
             args.putString("arabic", page.arabic)
             args.putString("translation", page.translation)
-            args.putInt("repetition", page.repetition)
+            args.putString("repetition", page.repetition)
 
             val fragment = DoaPageFragment()
             fragment.arguments = args
