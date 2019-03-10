9511c06783405377d6b2bfc48d2fa39d623c529d
Luqman Sungkar <loekmansungkar@gmail.com>
2019-03-10 19:13:54 +0700

    add main menu

--- app/src/main/java/id/luqman/al_matsurat/MainActivity.kt
+++ app/src/main/java/id/luqman/al_matsurat/MainActivity.kt
@@ -3,6 +3,8 @@ package id.luqman.al_matsurat
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import id.luqman.al_matsurat.adapter.DoaPagerAdapter
 import id.luqman.al_matsurat.data.DataHelper
 import org.jetbrains.anko.find
@@ -17,5 +19,23 @@ class MainActivity : AppCompatActivity() {
 
         val viewPager: ViewPager = find(R.id.doa_viewpager)
         viewPager.adapter = pagerAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+            when(item?.itemId){
+                R.id.setting_menu -> {
+                    //buka activity bary
+                    return true
+                }
+                R.id.about_menu -> {
+                    return true
+                }
+                else -> return super.onOptionsItemSelected(item)
+            }
     }
 }
